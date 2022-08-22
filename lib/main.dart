6c92af7a48bf58760055eb9e0e8ad5965d0850a7
gladeator <kingalikingali21@gmail.com>
2022-08-22 23:22:17 +0300

    second solution

--- lib/main.dart
+++ lib/main.dart
@@ -127,31 +127,45 @@ class MyHomePage2 extends StatelessWidget{
         body: Column(
           children: [
             const BackCard(),
-
             const Padding(
               padding: EdgeInsets.all(15.0),
               child: ContactCard(),
             ),
-            const Expanded(
-              child: ListContacts(),
-            ),
-            Container(
-              margin: const EdgeInsets.only(bottom: 25),
-              width: 180,
-              height: 70,
-              decoration: const BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            Expanded(
+              child: Stack(
                 children: [
-                  TextButton(
-                    onPressed: (){},
-                    child: const Icon(Icons.groups_outlined, color: Colors.black87, size: 30,),),
-                  TextButton(
-                    onPressed: (){},
-                    child: const Icon(Icons.settings, color: Colors.black87, size: 25,),
+                  const Expanded(child: ListContacts()),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 25),
+                      width: 180,
+                      height: 70,
+                      decoration: const BoxDecoration(
+                        color: Color(0xffFFFFFF),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0, 20),
+                            blurRadius: 15,
+                            spreadRadius: 10
+                          )
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            onPressed: (){},
+                            child: const Icon(Icons.groups_outlined, color: Colors.black87, size: 30,),),
+                          TextButton(
+                            onPressed: (){},
+                            child: const Icon(Icons.settings, color: Colors.black87, size: 25,),
+                          )
+                        ],
+                      ),
+                    ),
                   )
                 ],
               ),
